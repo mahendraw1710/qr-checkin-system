9aa8cd73b86c19c6648f8ec7dc64d8aadaf0f00f
Mahendra <75470990+mahendraw1710@users.noreply.github.com>
2026-06-24 04:52:05 +0000

    update a

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
+import { supabase } from "@/lib/supabase";
+import { SupabaseClient } from "@supabase/supabase-js";
 
 export default function Home() {
   useEffect(() => {
@@ -16,23 +18,36 @@ export default function Home() {
 
     scanner.render(
       (decodedText) => {
-        alert(`QR Detected: ${decodedText}`);
+        const checkUser = async (qrId: string) => {
+          const { data, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("qr_id", qrId)
+          .single();
+
+        if (error || !data ) {
+          alert("QR Not Registered");
+          return;
+        }
+
+        alert(`Welcome ${data.full_name}`);
+        };
       },
       () => {}
     );
 
     return () => {
-      scanner.clear();
+      scanner.clear().catch(() => {});
     };
   }, []);
 
   return (
-    <div className="p-5">
+    <main className="p-4">
       <h1 className="text-2xl font-bold mb-4">
         QR Check-In System
       </h1>
 
       <div id="reader"></div>
-    </div>
+    </main>
   );
 }
