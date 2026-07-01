1b9fe6df06e128e0634d1fd9763d7104d770ffe4
Mahendra <75470990+mahendraw1710@users.noreply.github.com>
2026-07-01 09:52:37 +0000

    edit User Card

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -12,7 +12,7 @@ export default function UserCard({
   return (
     <div className="bg-white rounded-xl shadow-lg p-6">
       <h2 className="font-bold text-xl mb-4">
-        Employee Information
+        Guest's Information
       </h2>
 
       <div className="space-y-3">
